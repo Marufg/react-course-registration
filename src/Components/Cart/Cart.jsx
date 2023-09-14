eee06df3979a79e591781457b264e8795605a839
Marufg <marufb572@gmail.com>
2023-09-14 22:58:15 +0600

    added title name

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 import "./Cart.css"
 
-const Cart = () => {
+const Cart = ({selectedTitle}) => {
+    console.log(selectedTitle);
     return (
         <div>
-        
+        <h3>Course Name</h3>
+        {selectedTitle.map((title) =>(
+                <li>{title.title}</li>
+        ))}
         </div>
     );
 };
